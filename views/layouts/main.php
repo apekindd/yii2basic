0dc4c0b269974e6b2cacf070cee3cfc7f4d6a672
apekin-pc\apekin <apekin@uamaster.com>
2016-09-28 11:07:52 +0300

    lesson 21 complete

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,6 +4,7 @@
 /* @var $content string */
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
@@ -36,10 +37,10 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Home', 'url' => ['/']],
             ['label' => 'Articles', 'url' => ['/post/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'About', 'url' => ['/about']],
+            ['label' => 'Contact', 'url' => ['/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
@@ -58,6 +59,11 @@ AppAsset::register($this);
     ?>
 
     <div class="container">
+
+        <?php echo Html::a('Home', Url::to('/'))?>
+        <?php echo Html::a('about', Url::to(['/about']))?>
+        <?php echo Html::a('contact', Url::to(['/contact']))?>
+
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
